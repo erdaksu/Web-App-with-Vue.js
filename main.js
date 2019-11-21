42f086eedbf2276725fc0cdcdcf2cb90a9df185e
erdaksu <erdemaksu77@gmail.com>
2019-11-21 18:41:36 +0000

    created a login and registration page that is stored onto localstorage

--- main.js
+++ main.js
@@ -42,70 +42,130 @@ var app = new Vue({
 })
 
 var vm = new Vue({
-	el:  "#filtercourses",
-	data: {
-		courses: [
-            { topic: 'Maths', location: 'Hendon', price: '£100'},
-            { topic: 'Maths', location: 'Colindale', price: '£80'},
-            { topic: 'Maths', location: 'Brent_Cross', price: '£90'},
-            { topic: 'Maths', location: 'Golders_Green', price: '£120'},
+    el: "#filtercourses",
+    data: {
+        courses: [
+            { topic: 'Maths', location: 'Hendon', price: '£100' },
+            { topic: 'Maths', location: 'Colindale', price: '£80' },
+            { topic: 'Maths', location: 'Brent_Cross', price: '£90' },
+            { topic: 'Maths', location: 'Golders_Green', price: '£120' },
 
-            { topic: 'English', location: 'Hendon', price: '£100'},
-            { topic: 'English', location: 'Colindale', price: '£80'},
-            { topic: 'English', location: 'Brent_Cross', price: '£90'},
-            { topic: 'English', location: 'Golders_Green', price: '£120'},
+            { topic: 'English', location: 'Hendon', price: '£100' },
+            { topic: 'English', location: 'Colindale', price: '£80' },
+            { topic: 'English', location: 'Brent_Cross', price: '£90' },
+            { topic: 'English', location: 'Golders_Green', price: '£120' },
 
-            { topic: 'Science', location: 'Hendon', price: '£100'},
-            { topic: 'Science', location: 'Colindale', price: '£80'},
-            { topic: 'Science', location: 'Brent_Cross', price: '£90'},
-            { topic: 'Science', location: 'Golders_Green', price: '£120'},
+            { topic: 'Science', location: 'Hendon', price: '£100' },
+            { topic: 'Science', location: 'Colindale', price: '£80' },
+            { topic: 'Science', location: 'Brent_Cross', price: '£90' },
+            { topic: 'Science', location: 'Golders_Green', price: '£120' },
 
-            { topic: 'Sports', location: 'Hendon', price: '£100'},
-            { topic: 'Sports', location: 'Colindale', price: '£80'},
-            { topic: 'Sports', location: 'Brent_Cross', price: '£90'},
-            { topic: 'Sports', location: 'Golders_Green', price: '£120'},
+            { topic: 'Sports', location: 'Hendon', price: '£100' },
+            { topic: 'Sports', location: 'Colindale', price: '£80' },
+            { topic: 'Sports', location: 'Brent_Cross', price: '£90' },
+            { topic: 'Sports', location: 'Golders_Green', price: '£120' },
 
-            ],
+        ],
         selectedCourse: "All",
         selectedLocation: "All",
         selectedPrice: "All"
-	},
-	computed: {
-		filteredCourses: function() {
-			var vm = this;
+    },
+    computed: {
+        filteredCourses: function () {
+            var vm = this;
             var topic = vm.selectedCourse;
 
-			if (topic=== "All") {
-				return vm.courses;
-			} else {
-				return vm.courses.filter(function(course) {
-					return course.topic === topic;
-				});
-			}
+            if (topic === "All") {
+                return vm.courses;
+            } else {
+                return vm.courses.filter(function (course) {
+                    return course.topic === topic;
+                });
+            }
         },
         filteredLocations: function () {
             var vm = this;
             var location = vm.selectedLocation;
 
-			if (location=== "All") {
-				return vm.courses;
-			} else {
-				return vm.courses.filter(function(course) {
-					return course.location === location;
-				});
-			}
+            if (location === "All") {
+                return vm.courses;
+            } else {
+                return vm.courses.filter(function (course) {
+                    return course.location === location;
+                });
+            }
         },
         filteredPrices: function () {
             var vm = this;
             var price = vm.selectedPrice;
 
-			if (price=== "All") {
-				return vm.courses;
-			} else {
-				return vm.courses.filter(function(course) {
-					return course.price === price;
-				});
-			}
+            if (price === "All") {
+                return vm.courses;
+            } else {
+                return vm.courses.filter(function (course) {
+                    return course.price === price;
+                });
+            }
         },
-	}
+    }
 });
+
+var signupApp = new Vue({
+    el: '#signup',
+    data: {
+        type: '',
+        email: '',
+        password: '',
+    },
+    methods: {
+        onSubmit: function () {
+            var users = '';
+            var newEmail = this.email;
+            if (localStorage.getItem('users')) { 
+                users = JSON.parse(localStorage.getItem('users'));
+            };
+            if (users) {
+                if (users.some(function (user) { return user.email === newEmail })) {
+                    alert('Email already exists!');
+                    return;
+                }
+                users.push({ 'type': this.type, 'email': newEmail, 'password': this.password });
+                localStorage.setItem('users', JSON.stringify(users));
+            }
+            else {
+                users = [{ 'type': this.type,'email': newEmail, 'password': this.password }];
+                localStorage.setItem('users', JSON.stringify(users));
+            }
+        }
+    }
+})
+
+var loginApp = new Vue({
+    el: '#login',
+    data: {
+        type: '',
+        email: '',
+        password: '',
+    },
+    methods: {
+        onSubmit: function () {
+            var users = '';
+            var newEmail = this.email;
+            if (localStorage.getItem('users')) { 
+                users = JSON.parse(localStorage.getItem('users'));
+            };
+            if (users) {
+                if (users.some(function (user) { return user.email === newEmail })) {
+                    alert('WELCOME!');
+                    return;
+                }
+                users.push({ 'type': this.type, 'email': newEmail, 'password': this.password });
+                localStorage.setItem('users', JSON.stringify(users));
+            }
+            else {
+                users = [{ 'type': this.type,'email': newEmail, 'password': this.password }];
+                localStorage.setItem('users', JSON.stringify(users));
+            }
+        }
+    }
+})
